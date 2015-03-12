8d227ed91c017377d5f3be4f46209e7f5924850e
pritstift <sascha.thiergart@ovgu.de>
2015-03-12 21:26:43 +0100

    included namespacing

--- IBID/IBID/urls.py
+++ IBID/IBID/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^ideas/', include('AddIdea.urls')),
+    url(r'^ideas/', include('AddIdea.urls',namespace="AddIdea")),
     url(r'^admin/', include(admin.site.urls)),
 )
