bd3013a1e972b49ec3c1347ff37f200fc405005d
IBIDOVGU <sascha.thiergart@web.de>
2015-03-28 19:39:05 +0100

    Update views.py

--- IBID/AddIdea/views.py
+++ IBID/AddIdea/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-
+from django.http import HttpResponse, HttpResponseRedirect 
+from datetime import datetime
+from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, render
-
+from AddIdea import models as m
 from AddIdea.models import Idea
 
 def detail(request, Idea_id):
@@ -16,3 +18,19 @@ def index(request):
         'latest_ideas': latest_ideas,
     })
     return HttpResponse(template.render(context))
+def post_detail(request, post_id):
+    try:
+        post = m.Post.objects.get(pk=post_id)
+    except m.Post.DoesNotExist:
+        # If no Post has id post_id, we raise an HTTP 404 error.
+        raise HttpResponse("Http404")
+    return render(request, 'detail.html', {'post': post})
+
+def post_upload(request):
+    if request.method == 'GET':
+        return render(request, 'post/upload.html', {})
+    elif request.method == 'POST':
+        post = m.Post.objects.create(content=request.POST['content'],
+                                     created_at=datetime.utcnow())
+  
+        return HttpResponseRedirect(reverse('post_detail', kwargs={'post_id': post.id}))
