006f9c6b5d9717bd0bfc719a63966357a39d28a2
Sascha Thiergart <sascha.thiergart@ovgu.de>
2015-04-14 17:34:30 +0200

    used django base.html for user login

--- IBID/IBID/urls.py
+++ IBID/IBID/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^ideas/', include('AddIdea.urls',namespace="AddIdea")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^users/', include('ManageUsers.urls', namespace="ManageUsers")),
+    url(r'^accounts/', include('ManageUsers.urls', namespace="ManageUsers")),
 )
