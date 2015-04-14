ae2222ed22becfcf9875c0a28e66233dee63ae76
pritstift <sascha.thiergart@ovgu.de>
2015-04-14 23:05:00 +0200

    logout

--- IBID/ManageUsers/urls.py
+++ IBID/ManageUsers/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     # ex: /users/sascha/
     url(r'^profile/(?P<User_username>\w+)/$', views.userprofile, name='userprofile'),
     url('^login/','django.contrib.auth.views.login',name='login'),
+    url('^logout/','django.contrib.auth.views.logout',{'next_page':'AddIdea:index'}, name='logout'),
 )
