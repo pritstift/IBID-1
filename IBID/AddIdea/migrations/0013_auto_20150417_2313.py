0df4177b1e0141b5f98e7ea05bd860785a68b3ff
pritstift <sascha.thiergart@ovgu.de>
2015-04-17 23:48:29 +0200

    Added Register page

---
+++ IBID/AddIdea/migrations/0013_auto_20150417_2313.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.utils.timezone import utc
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('AddIdea', '0012_auto_20150416_0910'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='comment',
+            name='created_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 4, 17, 21, 13, 21, 947457, tzinfo=utc)),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='idea',
+            name='date_added',
+            field=models.DateField(default=datetime.datetime(2015, 4, 17, 21, 13, 21, 945703, tzinfo=utc)),
+            preserve_default=True,
+        ),
+    ]
--- IBID/ManageUsers/urls.py
+++ IBID/ManageUsers/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^profile/(?P<User_username>\w+)/$', views.userprofile, name='userprofile'),
     url('^login/','django.contrib.auth.views.login', name='login'),
     url('^logout/','django.contrib.auth.views.logout',{'next_page':'AddIdea:index'}, name='logout'),
+    url(r'^register/$', views.register, name='register'),
 )
