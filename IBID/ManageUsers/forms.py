e929bd41e9f04cbe775c124651630497657ac494
Packebusch <tom.packe@gmx.de>
2015-05-28 23:25:17 +0200

    modified userprofile page

--- IBID/ManageUsers/forms.py
+++ IBID/ManageUsers/forms.py
@@ -19,3 +19,12 @@ class UserProfileForm(forms.ModelForm):
 class LoginForm(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput())
+
+class DisplayUserForm(forms.ModelForm):
+	username=forms.CharField(widget=forms.TextInput(attrs={'class':'disabled', 'readonly':'readonly'}))
+	last_name=forms.CharField(widget=forms.TextInput(attrs={'class':'disabled', 'readonly':'readonly'}))
+	first_name=forms.CharField(widget=forms.TextInput(attrs={'class':'disabled', 'readonly':'readonly'}))
+	email = forms.EmailField(widget=forms.TextInput(attrs={'class':'disabled', 'readonly':'readonly'}))
+	class Meta:
+		model = User
+		fields = ('username', 'email','first_name','last_name')
