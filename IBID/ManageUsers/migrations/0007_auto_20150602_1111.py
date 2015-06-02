e2889d629c25efb5b1683443da5d3526813d354f
Sascha Thiergart <sascha.thiergart@ovgu.de>
2015-06-02 12:56:34 +0200

    fixed bug regarding case sensitivity

---
+++ IBID/ManageUsers/migrations/0007_auto_20150602_1111.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ManageUsers', '0006_auto_20150601_1844'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='userprofile',
+            name='date_joined',
+            field=models.DateField(default=datetime.datetime(2015, 6, 2, 9, 11, 15, 834431, tzinfo=utc)),
+            preserve_default=True,
+        ),
+    ]
