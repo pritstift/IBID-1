eecc1990029afa65c2856bcaa5f2e0834ab00132
pritstift <sascha.thiergart@ovgu.de>
2015-06-04 22:15:56 +0200

    deleted foreignkey to tags from ideas

--- IBID/ManageIdea/migrations/0001_initial.py
+++ IBID/ManageIdea/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import taggit.managers
 import datetime
 from django.utils.timezone import utc
 from django.conf import settings
@@ -10,6 +11,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('taggit', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -17,33 +19,33 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Comment',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('message', models.TextField()),
-                ('created_at', models.DateTimeField(default=datetime.datetime(2015, 5, 29, 15, 18, 30, 169862, tzinfo=utc))),
+                ('created_at', models.DateTimeField(default=datetime.datetime(2015, 6, 4, 20, 15, 18, 822422, tzinfo=utc))),
             ],
         ),
         migrations.CreateModel(
             name='Idea',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=400, unique=True)),
-                ('date_added', models.DateField(default=datetime.datetime(2015, 5, 29, 15, 18, 30, 168998, tzinfo=utc))),
-                ('description_short', models.CharField(max_length=2048, default='this Idea has no short description yet')),
-                ('description_long', models.CharField(max_length=2048, default='this Idea has no long description yet')),
+                ('date_added', models.DateField(default=datetime.datetime(2015, 6, 4, 20, 15, 18, 820846, tzinfo=utc))),
+                ('description_short', models.CharField(default='this Idea has no short description yet', max_length=2048)),
+                ('description_long', models.CharField(default='this Idea has no long description yet', max_length=2048)),
                 ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='Tag',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('text', models.CharField(max_length=64)),
             ],
         ),
         migrations.AddField(
             model_name='idea',
             name='tags',
-            field=models.ForeignKey(to='ManageIdea.Tag'),
+            field=taggit.managers.TaggableManager(help_text='A comma-separated list of tags.', verbose_name='Tags', through='taggit.TaggedItem', to='taggit.Tag'),
         ),
         migrations.AddField(
             model_name='comment',
