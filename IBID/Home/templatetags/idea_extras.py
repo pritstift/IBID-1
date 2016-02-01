c26c45e1ba14d778780f4f87e693ac818499ad95
unknown <thiergar@BWL-IFM6.ww.uni-magdeburg.de>
2016-02-01 17:06:32 +0100

    fixed project and idea memberships on profile page. added requirements.txt; added styles.css; added centered tag for tablerows

--- IBID/Home/templatetags/idea_extras.py
+++ IBID/Home/templatetags/idea_extras.py
@@ -1,6 +1,9 @@
 from django import template 
 from guardian.shortcuts import get_perms
 from django.contrib.auth.models import Group
+from ManageIdea.models import Idea
+from ManageUsers.models import UserProfile
+from ManageConnections.models import Membership
 
 register = template.Library()
 
@@ -20,3 +23,17 @@ def has_group(user, group_name):
 @register.filter(name='get_task')
 def get_task(user, idea):
 	return user.ideamembership_set.get(idea=idea).task
+
+@register.filter(name='get_all_ideas')
+def get_all_ideas(user):
+	ideas=[]
+	for membership in Membership.objects.filter(member=user, project=None):
+		ideas.append(membership.idea)
+	return  ideas
+
+@register.filter(name='get_all_projects')
+def get_all_ideas(user):
+	projects=[]
+	for membership in Membership.objects.filter(member=user, idea=None):
+		projects.append(membership.project)
+	return  projects
